f083a431b603d448da64d9dcea6e225b8fccd507
OSVALDOJEQUE <osvaldojeque@gmail.com>
2024-07-18 14:25:50 +0200

    Update1

--- src/test_posts.py
+++ src/test_posts.py
@@ -1,20 +1,16 @@
 import requests
 import pytest
-from src.utils import BASE_URL, HEADERS, get_token
+from src.utils import BASE_URL, HEADERS
 
-@pytest.fixture(scope="module")
-def token():
-    # Supondo que get_token retorna um token válido
-    return get_token("user", "pass123")
 
 def test_create_post_valid_data(token):
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.post(BASE_URL + "/posts", json={"title": "Test Post", "content": "This is a test post."}, headers=headers)
+    response = requests.post(BASE_URL + "/posts", json={"title": "Test Post", "content": "This is a test post.", "public":True }, headers=headers)
     assert response.status_code == 201
 
 def test_update_post(token):
     headers = {"Authorization": f"Bearer {token}"}
-    response = requests.put(BASE_URL + "/posts/1", json={"title": "Updated Title", "content": "Updated content."}, headers=headers)
+    response = requests.put(BASE_URL + "/posts/1", json={"title": "Updated Title", "content": "Updated content.", "public":True}, headers=headers)
     assert response.status_code == 200
 
 def test_delete_post(token):
--- src/test_users.py
+++ src/test_users.py
@@ -2,7 +2,7 @@ import requests
 from src.utils import BASE_URL, HEADERS
 
 def test_create_user_valid_data():
-    response = requests.post(BASE_URL + "/users", json={"username": "newUser", "password": "newPass"}, headers=HEADERS)
+    response = requests.post(BASE_URL + "/users", data={"username": "newUser","email":"neuser@example.com","full_name":"full_name","disabled":False , "password": "newPass"}, headers=HEADERS)
     assert response.status_code == 201
 
 def test_create_user_invalid_data():
