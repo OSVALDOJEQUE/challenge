59107ee7da54e587d85398b811f8a8319846e21f
OSVALDOJEQUE <osvaldojeque@gmail.com>
2024-07-18 14:45:54 +0200

    Update1

--- src/test_users.py
+++ src/test_users.py
@@ -2,9 +2,17 @@ import requests
 from src.utils import BASE_URL, HEADERS
 
 def test_create_user_valid_data():
-    response = requests.post(BASE_URL + "/users", data={"username": "newUser","email":"neuser@example.com","full_name":"full_name","disabled":False , "password": "newPass"}, headers=HEADERS)
+    data = {
+    "email": "u@example.com",
+    "username": "string",
+    "full_name": "string",
+    "password": "string"
+    }
+    
+    response = requests.post(BASE_URL + "/users", json=data, headers=HEADERS)
     assert response.status_code == 201
 
 def test_create_user_invalid_data():
-    response = requests.post(BASE_URL + "/users", json={"username": "", "password": ""}, headers=HEADERS)
+    data = {"username": "", "password": "","full_name":"", "password": ""}
+    response = requests.post(BASE_URL + "/users", data=data, headers=HEADERS)
     assert response.status_code == 400
